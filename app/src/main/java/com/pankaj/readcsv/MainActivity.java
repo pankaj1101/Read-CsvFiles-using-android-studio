720a9c92287e8567dbec622afba1446a021e1e55
pankaj <pankajram1101@gmail.com>
2022-12-28 18:02:46 +0530

    Final

---
+++ app/src/main/java/com/pankaj/readcsv/MainActivity.java
@@ -0,0 +1,69 @@
+package com.pankaj.readcsv;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String[] data;
+    private TextView text;
+    private EditText input;
+    private Button btn_click;
+    private String name;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        text = findViewById(R.id.text);
+        input = findViewById(R.id.inputField);
+        btn_click = findViewById(R.id.click_me);
+
+        btn_click.setOnClickListener(v -> {
+            name = input.getText().toString().toLowerCase(Locale.ROOT).trim();
+            //Method to fetch the value from csv file and display on TextView
+            display_data(name);
+        });
+    }
+
+    private void display_data(String name) {
+
+        //data is my csv file into raw folder.
+        //You can change the csv file.
+        //You can also Update the csv file.
+
+        InputStream inputStream = getResources().openRawResource(R.raw.data);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        try {
+            String csvLine;
+            while ((csvLine = reader.readLine()) != null) {
+                data = csvLine.split(",");
+
+                try {
+                    if (data[0].equals(name)) {
+                        text.setText("Age: " + data[1]);
+                        break;
+                    } else {
+                        text.setText("Not Found in Dataset !.. ");
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
